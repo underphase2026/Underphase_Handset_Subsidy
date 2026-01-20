62dbae775a8e8bda7294646524b04ccfa6f35b89
oxoxoxoxox <dudwlsaos132@naver.com>
2026-01-20 11:30:34 +0900

    initial commit

---
+++ src/main/java/com/example/Underphase_Handset/Subsidy/UnderphaseHandsetSubsidyApplication.java
@@ -0,0 +1,12 @@
+package com.example.Underphase_Handset.Subsidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderphaseHandsetSubsidyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderphaseHandsetSubsidyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/Underphase_Handset/Subsidy/UnderphaseHandsetSubsidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Underphase_Handset.Subsidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderphaseHandsetSubsidyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
