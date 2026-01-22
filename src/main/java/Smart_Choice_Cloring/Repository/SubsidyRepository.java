8ddae3a5fb0c1e81709ce6ad69575be4bd87b51e
oxoxoxoxox <dudwlsaos132@naver.com>
2026-01-22 11:12:12 +0900

    [Add] 공시지원금에 대한 레퍼지토리 추가

---
+++ src/main/java/Smart_Choice_Cloring/Repository/SubsidyRepository.java
@@ -0,0 +1,6 @@
+package Smart_Choice_Cloring.Repository;
+import Smart_Choice_Cloring.Entity.Subsidy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubsidyRepository extends JpaRepository<Subsidy, Long> {
+}
