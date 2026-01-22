0b080e8591a43e273cd26bd898230692fb09d26a
oxoxoxoxox <dudwlsaos132@naver.com>
2026-01-22 11:10:42 +0900

    [Add] 공시지원금에 대한 Entity 구성

--- src/main/java/Smart_Choice_Cloring/Entity/Subsidy.java
+++ src/main/java/Smart_Choice_Cloring/Entity/Subsidy.java
@@ -1,2 +1,37 @@
-package Smart_Choice_Cloring.Entity;public class Subsidy {
+package Smart_Choice_Cloring.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Subsidy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String telecom;      // 통신사 (SKT, KT, LG U+)
+    private String deviceName;   // 단말기 명
+    private String planName;     // 요금제 명
+    private String supportAmount; // 공시지원금
+
+    private LocalDateTime updatedAt; // 데이터 수집 시간
+
+    @Builder
+    public Subsidy(String telecom, String deviceName, String planName, String supportAmount) {
+        this.telecom = telecom;
+        this.deviceName = deviceName;
+        this.planName = planName;
+        this.supportAmount = supportAmount;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
