6b0c40f92aa498c933f2044ec7783213fbd3f685
oxoxoxoxox <dudwlsaos132@naver.com>
2026-01-22 11:13:57 +0900

    [Add] 공시지원금에 대한 컨트롤러 파일 추가

---
+++ src/main/java/Smart_Choice_Cloring/Controller/SubsidyController.java
@@ -0,0 +1,20 @@
+package Smart_Choice_Cloring.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/subsidy")
+@RequiredArgsConstructor
+public class SubsidyController {
+
+    private final SubsidyService subsidyService;
+
+    @GetMapping("/crawl")
+    public String triggerCrawling() {
+        subsidyService.crawlAndSaveSubsidies();
+        return "크롤링 및 데이터 저장이 완료되었습니다.";
+    }
+}
