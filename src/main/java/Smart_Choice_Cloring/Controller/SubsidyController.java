71f0d3d9d7678378f0815d8e66b5d06b21168cd1
oxoxoxoxox <dudwlsaos132@naver.com>
2026-01-23 12:23:19 +0900

    [Edit] 스마트초이스 크롤링에 해당하는 엔드포인트 변경

--- src/main/java/Smart_Choice_Cloring/Controller/SubsidyController.java
+++ src/main/java/Smart_Choice_Cloring/Controller/SubsidyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/subsidy")
+@RequestMapping("/smartchoice")
 @RequiredArgsConstructor
 public class SubsidyController {
 
